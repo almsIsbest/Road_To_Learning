1aff2338377e15bba42e6c3251e74010a3cb53d3
alimasi <alimasi@fotoable.com>
2021-12-07 11:28:40 +0800

    edit

---
+++ JavaBasicLearning/src/main/java/schedule/Scheduledemo.java
@@ -0,0 +1,26 @@
+package schedule;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName Scheduledemo
+ * @Description TODO
+ * @Author Scheduledemo
+ * @Data 2021/11/26 18:35
+ **/
+public class Scheduledemo {
+    public static void main(String[] args) {
+        ScheduledExecutorService res= Executors.newScheduledThreadPool(1);
+
+        Runnable task2=()-> System.out.println("Running task2");
+
+        ScheduledFuture<?> result=res.schedule(task2,5, TimeUnit.SECONDS);
+        if(result!=null&&result.isCancelled()){
+            System.out.println("已经取消");
+        }
+    }
+
+}
