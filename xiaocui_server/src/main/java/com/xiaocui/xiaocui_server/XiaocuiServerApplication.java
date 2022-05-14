da5e69f0ff3eea19f861f7337f4de85fdddb5d52
alimasi <alimasi@fotoable.com>
2022-05-15 01:04:55 +0800

    web test

---
+++ xiaocui_server/src/main/java/com/xiaocui/xiaocui_server/XiaocuiServerApplication.java
@@ -0,0 +1,15 @@
+package com.xiaocui.xiaocui_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaocuiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaocuiServerApplication.class, args);
+    }
+
+
+
+}
---
+++ xiaocui_server/src/test/java/com/xiaocui/xiaocui_server/XiaocuiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaocui.xiaocui_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaocuiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
